d1c7bbad5baed444aa9af18e5c492029da1f4a38
Robert Nowak <robert.andreas.nowak@gmail.com>
2016-01-16 21:44:24 +0100

    added toggle jquery to all database entries appear and disappear

--- Final_Project_BE/m1c2cmp/app/assets/javascripts/application.js
+++ Final_Project_BE/m1c2cmp/app/assets/javascripts/application.js
@@ -19,8 +19,18 @@ $( document ).ready(function() {
   
 	$('.hamburgermenu').on('click', function() {
     	$(this).next().toggleClass("active");
+    	console.log('click');
+    	
 
 	});
 
-	
+	$('#database_table_image').click(function(){
+		$('.database_global').toggleClass("visible");
+		console.log('click');
+	});
+
+
+// Toggle for logos - end
+
+
 });
